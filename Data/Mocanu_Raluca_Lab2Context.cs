73028ec23932e36fe2c1696bb28c42625e264912
Raluca <Raluca@DESKTOP-1OO1OKA>
2023-10-22 22:18:15 +0300

    Add project files.

---
+++ Data/Mocanu_Raluca_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mocanu_Raluca_Lab2.Models;
+
+namespace Mocanu_Raluca_Lab2.Data
+{
+    public class Mocanu_Raluca_Lab2Context : DbContext
+    {
+        public Mocanu_Raluca_Lab2Context (DbContextOptions<Mocanu_Raluca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mocanu_Raluca_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Mocanu_Raluca_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
